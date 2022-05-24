df45e1fd8f2ffec2440214c48577366de74018aa
Ajeet Singh <ajeet2dx@gmail.com>
2022-05-23 23:53:37 -0700

    second commit

--- prime.py
+++ prime.py
@@ -1,20 +1,20 @@
-# A default function for Prime checking conditions  
-def PrimeChecker(a):  
-    # Checking that given number is more than 1  
-    if a > 1:  
-        # Iterating over the given number with for loop  
-        for j in range(2, int(a/2) + 1):  
-            # If the given number is divisible or not  
-            if (a % j) == 0:  
-                print(a, "is not a prime number")  
-                break  
-        # Else it is a prime number  
-        else:  
-            print(a, "is a prime number")  
-    # If the given number is 1  
-    else:  
-        print(a, "is not a prime number")  
-# Taking an input number from the user  
-a = int(input("Enter an input number:"))  
-# Printing result  
-PrimeChecker(a)  
+# Prime number program by Ajeet Singh
+
+def prime(n):
+    for i in range(2,n+1):
+        if(n % i == 0):
+            break
+    if (n == i):
+        print(n,'is a Prime number')
+    else:
+        print(n,'is not a Prime number')
+
+def main():
+    n = int(input('Enter a number : '))
+    prime(n)
+
+if __name__ == '__main__':
+    main()
+
+
+
